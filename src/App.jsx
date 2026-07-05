e0a52e787806a4e5cf6b54f1c1f43d134b3f022a
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-05 08:12:33 +0530

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar.jsx'
+import Hero from './Hero/Hero.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero/>
     </div>
   )
 }
---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.css'
+import { Dot } from 'lucide-react';
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className="my_field">
+                <p className='field_name'> <Dot /> CS Student Engineer</p>
+            </div>
+            <div className="hero_con">
+                <h1>Hi, I'm Mishra</h1>
+                <h1>Frontend Developer & Student</h1>
+                <h3>I create modern, responsive, and user-friendly web applications using React, JavaScript, Tailwind CSS, and other modern web technologies.</h3>
+            </div>
+            <div className="buttons">
+                <button className="contact_me">Contact Me</button>
+                <button className="my_work">My Work</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
