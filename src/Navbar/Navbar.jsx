b0ebdd8477c4f88f9e52dc11153b7135a7a930fc
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-05 07:48:09 +0530

    navbar done

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+import { FolderKanban } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="name">Sahil_Mishra Portfolio</div>
+        <div className="links">
+            <a href="#projects">Projects</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+            <FolderKanban size={20} />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
